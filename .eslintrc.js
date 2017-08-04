6382792a452b2ce9f2469d6d5f596574b9181114
Michael Gauthier <mike@silverorange.com>
2017-08-04 17:56:32 -0300

    Add ESLint standard and update package metadata

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    browser: true,
+  },
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      },
+    ],
+  },
+};
