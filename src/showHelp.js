101b3b4c28f51d6edc97275d40ea50daf334a614
Ross Evans <therealrossevans@gmail.com>
2017-08-07 11:26:23 -0300

    Alter show help description to be more accurate

--- src/showHelp.js
+++ src/showHelp.js
@@ -4,7 +4,7 @@ module.exports = function showHelp() {
   console.log();
   console.log('Arguments:');
   console.log('  auth_token  a valid GitHub OAuth token used to get pull request dependencies.');
-  console.log('  job_name    string containing repository name and pull request number. For example site/pulls/238.');
+  console.log('  job_name    string containing organization name, repository name, and pull request number. For example silverorange/site/pulls/238.');
   console.log('  package     whether or not the pull request is for a package or a site.');
   console.log();
 };
