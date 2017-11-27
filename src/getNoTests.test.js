ba704f479f3edd9e4962c4f665cb17c43f5f409a
Michael Gauthier <mike@silverorange.com>
2017-11-27 10:11:10 -0400

    Add tests for getNoTests function

---
+++ src/getNoTests.test.js
@@ -0,0 +1,77 @@
+const getNoTests = require('./getNoTests');
+
+test('no tests one space', () => {
+  expect(getNoTests('no tests')).toBe(true);
+});
+
+test('no tests two spaces', () => {
+  expect(getNoTests('no  tests')).toBe(true);
+});
+
+test('no tests tabs', () => {
+  expect(getNoTests('no\ttests')).toBe(true);
+});
+
+test('emoji one space', () => {
+  expect(getNoTests('🚫 tests')).toBe(true);
+});
+
+test('emoji two spaces', () => {
+  expect(getNoTests('🚫  tests')).toBe(true);
+});
+
+test('emoji tabs', () => {
+  expect(getNoTests('🚫\ttests')).toBe(true);
+});
+
+test('do not run tests', () => {
+  expect(getNoTests('do not run tests')).toBe(true);
+});
+
+test('don\'t run tests', () => {
+  expect(getNoTests('don\'t run tests')).toBe(true);
+});
+
+test('uppercase no tests', () => {
+  expect(getNoTests('No tests')).toBe(true);
+});
+
+test('uppercase emoji ', () => {
+  expect(getNoTests('🚫 Tests')).toBe(true);
+});
+
+test('uppercase do not run tests', () => {
+  expect(getNoTests('Do not run tests.')).toBe(true);
+});
+
+test('uppercase don\'t run tests', () => {
+  expect(getNoTests('Don\'t run tests.')).toBe(true);
+});
+
+test('no tests in sentence', () => {
+  expect(getNoTests('please no tests for this PR')).toBe(true);
+});
+
+test('emoji in sentence', () => {
+  expect(getNoTests('please 🚫 tests for this PR')).toBe(true);
+});
+
+test('do not run tests in sentence', () => {
+  expect(getNoTests('please do not run tests for this PR')).toBe(true);
+});
+
+test('don\'t run tests in sentence', () => {
+  expect(getNoTests('pease don\'t run tests for this PR')).toBe(true);
+});
+
+test('run tests', () => {
+  expect(getNoTests('run tests')).toBe(false);
+});
+
+test('tests', () => {
+  expect(getNoTests('tests')).toBe(false);
+});
+
+test('(blank)', () => {
+  expect(getNoTests('')).toBe(false);
+});
