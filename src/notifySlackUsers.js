90d80ee8a5afc69b3d904a885f577c592e9f6c6b
Ross Evans <therealrossevans@gmail.com>
2017-12-06 12:07:53 -0500

    Fix linting, add proper message formatting

--- src/notifySlackUsers.js
+++ src/notifySlackUsers.js
@@ -1,40 +1,57 @@
+/* eslint-disable no-unused-vars */
+
 const Slack = require('slack');
 const Promise = require('bluebird');
-const bot = new Slack({token: process.env.SLACK_BOT_TOKEN});
+
+const bot = new Slack({ token: process.env.SLACK_BOT_TOKEN });
 
 function getActiveOrganizationUsers() {
-  return Promise.resolve(bot.users.list({}).then(data => {
-    return data.members.filter(user => !user.deleted);
-  }));
+  return Promise.resolve(
+    bot.users.list({}).then(data => data.members.filter(user => !user.deleted))
+  );
 }
 
 module.exports = function notifySlackUser(user, buildInformation, callback) {
-  getActiveOrganizationUsers().then(slackUsers => {
-    realNameMatch = slackUsers.filter(slackUser => user.displayName === slackUser.real_name);
-    userNameMatch = slackUsers.filter(slackUser => user.userName === slackUser.name);
+  getActiveOrganizationUsers()
+    .then(slackUsers => {
+      const realNameMatch = slackUsers.filter(
+        slackUser => user.displayName === slackUser.real_name
+      );
+      const userNameMatch = slackUsers.filter(
+        slackUser => user.userName === slackUser.name
+      );
 
-    if (realNameMatch.length === 0 && userNameMatch.length === 0) {
-      console.log('Unable to match GitHub user to Slack user');
-      process.exit(1);
-    }
-
-    userToContact = realNameMatch.length > 0 ? realNameMatch[0] : userNameMatch[0];
-    bot.im.open({user: userToContact.id}).then(response => {
-      if (!response.ok) {
-        console.log(`Error opening Slack Message Channel with ${userToContact.real_name}`);
-        console.log(response);
-        process.exit(1);
+      if (realNameMatch.length === 0 && userNameMatch.length === 0) {
+        callback('Unable to match GitHub user to Slack user');
       }
 
-      bot.chat.postMessage({channel: response.channel.id, text: 'test'}, console.log);
-    }).catch(err => {
-      console.log(err);
-      process.exit(1);
+      const userToContact =
+        realNameMatch.length > 0 ? realNameMatch[0] : userNameMatch[0];
+      bot.im
+        .open({ user: userToContact.id })
+        .then(response => {
+          if (!response.ok) {
+            console.log(
+              `Error opening Slack Message Channel with ${
+                userToContact.real_name
+              }`
+            );
+            callback(response);
+          }
+
+          bot.chat
+            .postMessage({
+              channel: response.channel.id,
+              text: `Your PR _${buildInformation.prName}_ in _${
+                buildInformation.repoName
+              }_ failed CI during the _${buildInformation.stageName}_ stage. <${
+                buildInformation.ciLink
+              }|View in Jenkins>`
+            })
+            .then(data => callback(null, 'Success!'))
+            .catch(err => callback(err));
+        })
+        .catch(err => callback(err));
     })
-
-  }).catch(err => {
-    console.log(err);
-    process.exit(1);
-  });
-
-}
+    .catch(err => callback(err));
+};
