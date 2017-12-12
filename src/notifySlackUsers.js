efe786d0a2e9bcf24d4a8a8b51b4ab836e014cea
Ross Evans <therealrossevans@gmail.com>
2017-12-11 23:32:06 -0500

    Add tests, restructure code to work around tests

--- src/notifySlackUsers.js
+++ src/notifySlackUsers.js
@@ -11,47 +11,44 @@ function getActiveOrganizationUsers() {
   );
 }
 
+function contactUser(user, buildInformation, callback) {
+  bot.im.open({ user: user.id }).then(response => {
+    if (!response.ok) {
+      console.log(`Error opening Slack Message Channel with ${user.real_name}`);
+      callback(response);
+    }
+
+    bot.chat
+      .postMessage({
+        channel: response.channel.id,
+        text: `Your PR _${buildInformation.prName}_ in _${
+          buildInformation.repoName
+        }_ failed CI during the _${buildInformation.stageName}_ stage. <${
+          buildInformation.ciLink
+        }|View in Jenkins>`
+      })
+      .then(data => callback(null, 'Success!'))
+      .catch(err => callback(err));
+  });
+}
+
 module.exports = function notifySlackUser(user, buildInformation, callback) {
-  getActiveOrganizationUsers()
-    .then(slackUsers => {
-      const realNameMatch = slackUsers.filter(
-        slackUser => user.displayName === slackUser.real_name
+  getActiveOrganizationUsers().then(slackUsers => {
+    const realNameMatch = slackUsers.filter(
+      slackUser => user.displayName === slackUser.real_name
+    );
+    const userNameMatch = slackUsers.filter(
+      slackUser => user.userName === slackUser.name
+    );
+
+    if (realNameMatch.length !== 0 || userNameMatch.length !== 0) {
+      contactUser(
+        realNameMatch.length > 0 ? realNameMatch[0] : userNameMatch[0],
+        buildInformation,
+        callback
       );
-      const userNameMatch = slackUsers.filter(
-        slackUser => user.userName === slackUser.name
-      );
-
-      if (realNameMatch.length === 0 && userNameMatch.length === 0) {
-        callback('Unable to match GitHub user to Slack user');
-      }
-
-      const userToContact =
-        realNameMatch.length > 0 ? realNameMatch[0] : userNameMatch[0];
-      bot.im
-        .open({ user: userToContact.id })
-        .then(response => {
-          if (!response.ok) {
-            console.log(
-              `Error opening Slack Message Channel with ${
-                userToContact.real_name
-              }`
-            );
-            callback(response);
-          }
-
-          bot.chat
-            .postMessage({
-              channel: response.channel.id,
-              text: `Your PR _${buildInformation.prName}_ in _${
-                buildInformation.repoName
-              }_ failed CI during the _${buildInformation.stageName}_ stage. <${
-                buildInformation.ciLink
-              }|View in Jenkins>`
-            })
-            .then(data => callback(null, 'Success!'))
-            .catch(err => callback(err));
-        })
-        .catch(err => callback(err));
-    })
-    .catch(err => callback(err));
+    } else {
+      callback('Unable to match GitHub user to Slack user');
+    }
+  });
 };
