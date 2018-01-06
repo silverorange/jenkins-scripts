0b50d67d2b4aa81f0547dcd18fc4cc026aa2b969
Michael Gauthier <mike@silverorange.com>
2018-01-06 17:53:49 -0400

    Use getActiveUser module

--- src/notifySlackUsers.js
+++ src/notifySlackUsers.js
@@ -1,24 +1,13 @@
-const getActiveOrganizationUsers = require('./slack/getActiveOrganizationUsers');
+const getActiveUser = require('./slack/getActiveUser');
 const contactUser = require('./slack/contactUser');
 
-module.exports = function notifySlackUser(user, buildInformation, callback) {
-  getActiveOrganizationUsers().then(slackUsers => {
-    const realNameMatch = slackUsers.filter(
-      slackUser => user.displayName === slackUser.real_name
-    );
-    const userNameMatch = slackUsers.filter(
-      slackUser => user.userName === slackUser.name
-    );
-
-    if (realNameMatch.length !== 0 || userNameMatch.length !== 0) {
-      contactUser(
-        realNameMatch.length > 0 ? realNameMatch[0] : userNameMatch[0],
-        buildInformation
-      )
-        .then(result => callback(null, result))
-        .catch(err => callback(err));
-    } else {
-      callback('Unable to match GitHub user to Slack user');
-    }
-  });
+module.exports = function notifySlackUser(
+  gitHubUser,
+  buildInformation,
+  callback
+) {
+  getActiveUser(gitHubUser)
+    .then(slackUser => contactUser(slackUser, buildInformation))
+    .then(result => callback(null, result))
+    .catch(err => callback(err));
 };
