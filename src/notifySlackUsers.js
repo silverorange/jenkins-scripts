4f1fa83231f37343b5adf917788d0447055d6716
Michael Gauthier <mike@silverorange.com>
2018-01-06 17:31:26 -0400

    Use slack modules in notifiy-slack-users

--- src/notifySlackUsers.js
+++ src/notifySlackUsers.js
@@ -1,34 +1,5 @@
-const Slack = require('slack');
-const Promise = require('bluebird');
-
-const bot = new Slack({ token: process.env.SLACK_BOT_TOKEN });
-
-function getActiveOrganizationUsers() {
-  return Promise.resolve(
-    bot.users.list({}).then(data => data.members.filter(user => !user.deleted))
-  );
-}
-
-function contactUser(user, buildInformation, callback) {
-  bot.im.open({ user: user.id }).then(response => {
-    if (!response.ok) {
-      console.log(`Error opening Slack Message Channel with ${user.real_name}`);
-      callback(response);
-    }
-
-    bot.chat
-      .postMessage({
-        channel: response.channel.id,
-        text: `Your PR _${buildInformation.prName}_ in _${
-          buildInformation.repoName
-        }_ failed CI during the _${buildInformation.stageName}_ stage. <${
-          buildInformation.ciLink
-        }|View in Jenkins>`
-      })
-      .then(() => callback(null, 'Success!'))
-      .catch(err => callback(err));
-  });
-}
+const getActiveOrganizationUsers = require('./slack/getActiveOrganizationUsers');
+const contactUser = require('./slack/contactUser');
 
 module.exports = function notifySlackUser(user, buildInformation, callback) {
   getActiveOrganizationUsers().then(slackUsers => {
@@ -42,9 +13,10 @@ module.exports = function notifySlackUser(user, buildInformation, callback) {
     if (realNameMatch.length !== 0 || userNameMatch.length !== 0) {
       contactUser(
         realNameMatch.length > 0 ? realNameMatch[0] : userNameMatch[0],
-        buildInformation,
-        callback
-      );
+        buildInformation
+      )
+        .then(result => callback(null, result))
+        .catch(err => callback(err));
     } else {
       callback('Unable to match GitHub user to Slack user');
     }
